30103c4ad4933812de0017d9f6aa827fdd4e6ce1
jsluna19 <Juan.luna@tau.usbmed.edu.co>
2025-11-11 23:22:15 -0500

    Creacion proyecto

---
+++ backend/src/main/java/com/red_comunitaria/RedComunitariaApplication.java
@@ -0,0 +1,13 @@
+package com.red_comunitaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedComunitariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedComunitariaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/red_comunitaria/RedComunitariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.red_comunitaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedComunitariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
