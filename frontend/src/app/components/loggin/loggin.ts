0b18bbd3fc26328afbe02c9ea9e5076574751976
jsluna19 <Juan.luna@tau.usbmed.edu.co>
2025-11-18 23:25:10 -0500

    Creacion componentes

---
+++ frontend/src/app/components/loggin/loggin.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loggin',
+  imports: [],
+  templateUrl: './loggin.html',
+  styleUrl: './loggin.css',
+})
+export class Loggin {
+
+}
