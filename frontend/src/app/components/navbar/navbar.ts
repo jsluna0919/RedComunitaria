1d14386a89e38dbffac192b6183d220b8e84e109
Natalia F <natalia.florezm1@udea.edu.co>
2025-12-03 16:14:46 -0500

    changes

--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
+  standalone: true,
 })
 export class Navbar {
 
